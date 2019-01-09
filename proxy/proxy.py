ba59401c8a63413583fa321b9d2e1f3a5893c97a
SilverW0o0W <silver.codingcat@gmail.com>
2019-01-09 16:16:28 +0800

    Update proxy, add validator and fix bug

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -5,18 +5,16 @@ from proxy import const
 
 class Proxy(object):
 
-    def __init__(self, ip, port, protocol, score=0, created_time=None, verified_time=None):
+    def __init__(self, ip, port, protocol):
         self.ip = ip
         self.port = port
         self.protocol = protocol
-        self.score = score
-        self.created_time = created_time
-        self.verified_time = verified_time
 
     def __str__(self):
         return "{}:{}".format(self.ip, self.port)
 
     def to_proxies(self):
+        protocol = "http" if self.protocol == const.HTTP else "https"
         return {
-            "http" if self.protocol == const.HTTP else "https": str(self)
+            protocol: "{}://{}".format(protocol, self)
         }
