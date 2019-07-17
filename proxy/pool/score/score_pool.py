8c25546611811c89b5b6f43f74976768a30000f0
SilverW0o0W <silver.codingcat@gmail.com>
2019-07-17 19:35:50 +0800

    Update proxy

--- proxy/pool/score/score_pool.py
+++ proxy/pool/score/score_pool.py
@@ -4,8 +4,20 @@
 """
 This is for crawling proxy ip from ip website.
 """
+import time
+
 from proxy import const
+from proxy.proxy import Proxy
 from proxy.pool.pool import PoolBase
+
+
+class ScoreProxy(Proxy):
+
+    def __init__(self, ip, port, protocol, score, created_time, updated_time):
+        super().__init__(ip, port, protocol)
+        self.score = score
+        self.created_time = created_time
+        self.updated_time = updated_time
 
 
 class ScorePool(PoolBase):
@@ -34,5 +46,16 @@ class ScorePool(PoolBase):
 
     def crawl(self):
         status, msg, proxies = self.spider.get_proxies()
-        for proxy in proxies:
-            status = self.validator.check_proxy(proxy)
+        now = time.time()
+        checked_proxies = [
+            self.build_score_proxy(proxy, 20, now, now)
+            for proxy in proxies
+            if self.validator.check_proxy(proxy) == const.SUCCESS
+        ]
+
+    @staticmethod
+    def build_score_proxy(proxy, score, created_time, updated_time):
+        return ScoreProxy(
+            proxy.ip, proxy.port, proxy.protocol,
+            score, created_time, updated_time
+        )
